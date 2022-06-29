fcc99a25677692be72eea7cd9edb5d67b2284fd0
Muhammet ali Iskirik <miskirik@k1m43s06.42istanbul.com.tr>
2022-06-29 05:49:29 +0300

    changes applied

--- client.c
+++ client.c
@@ -6,7 +6,20 @@
 /*   By: miskirik <miskirik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:36:59 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/28 22:37:00 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/06/29 01:41:21 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+# include "ft_printf.h"
+#include <unistd.h>
+
+int main(int argc,char **argv)
+{
+	if(argc != 3)
+	{
+		ft_printf("You failed.");
+		return(0);
+	}
+	ft_printf("%d",getpid());
+}
--- server.c
+++ server.c
@@ -6,7 +6,35 @@
 /*   By: miskirik <miskirik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:36:56 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/28 22:36:57 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/06/29 03:16:03 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include "ft_printf.h"
+
+void ft_server()
+{
+
+}
+
+int main(void)
+{
+	struct sigaction	server;
+	int pid;
+
+	server.sa_flags= SA_SIGINFO;
+	server.sa_sigaction= ft_server;
+	if (sigaction(SIGUSR1,&server,0)== -1)
+		ft_printf("Signal Error\n");
+	if (sigaction(SIGUSR2,&server,0)== -1)
+		ft_printf("Signal Error\n");
+	pid=getpid();
+	ft_printf("Server pid=%d\n",pid);
+
+	while (1)
+		pause();
+	return(0);
+
+}
