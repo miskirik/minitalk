9fbb4f5cc07948f96b03091ea682183b16471b11
Muhammet ali Iskirik <miskirik@k1m09s02.42istanbul.com.tr>
2022-07-01 06:00:29 +0300

    finish

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: miskirik <miskirik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:36:59 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/29 01:41:21 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/07/01 04:49:21 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,54 @@
 # include "ft_printf.h"
 #include <unistd.h>
 
+void ft_ok(int signal)
+{
+	if (signal == SIGUSR1)
+		ft_printf("Process Successful");
+}
+int	ft_atoi(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str)
+		i = (i * 10) + (*str++ - '0');
+	return (i);
+}
+
+void ft_procces(int pid,char *str)
+{
+	int i;
+	char c;
+
+	i=0;
+	c=0;
+	while (str[c])
+	{
+		i=7;
+		while (i>=0)
+		{
+			if(str[c] >> i & 1)
+				kill(pid,SIGUSR1);
+			else
+				kill(pid,SIGUSR2);
+			i--;
+			usleep(50);
+		}
+		c++;
+	}
+}
 int main(int argc,char **argv)
 {
+	int pid;
+
+	pid=ft_atoi(argv[1]);
+	//ft_printf("%d",pid);
 	if(argc != 3)
 	{
-		ft_printf("You failed.");
+		ft_printf("You failed.\n");
 		return(0);
 	}
-	ft_printf("%d",getpid());
+	signal (SIGUSR1, ft_ok);
+	ft_procces(pid, argv[2]);
 }
--- server.c
+++ server.c
@@ -6,17 +6,35 @@
 /*   By: miskirik <miskirik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:36:56 by miskirik          #+#    #+#             */
-/*   Updated: 2022/06/29 03:16:03 by miskirik         ###   ########.fr       */
+/*   Updated: 2022/07/01 04:48:40 by miskirik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
-#include <unistd.h>
 #include "ft_printf.h"
+#include <unistd.h>
 
-void ft_server()
+void	ft_server(int signal, siginfo_t *info, void *context)
 {
+	static int	i = 0;
+	static char	c = 0;
 
+	(void)context;
+	if(signal == SIGUSR1)
+		c = c | 1;
+	if (++i == 8)
+	{
+		i=0;
+		if (!c)
+		{
+			kill(info->si_pid,SIGUSR1);
+			ft_printf("\n");
+		}
+		ft_printf("%c",c);
+		c=0;
+	}
+	else
+		c <<=1;
 }
 
 int main(void)
@@ -24,15 +42,14 @@ int main(void)
 	struct sigaction	server;
 	int pid;
 
+	pid=getpid();
+	ft_printf("Server pid=%d\n",pid);
 	server.sa_flags= SA_SIGINFO;
 	server.sa_sigaction= ft_server;
 	if (sigaction(SIGUSR1,&server,0)== -1)
 		ft_printf("Signal Error\n");
 	if (sigaction(SIGUSR2,&server,0)== -1)
 		ft_printf("Signal Error\n");
-	pid=getpid();
-	ft_printf("Server pid=%d\n",pid);
-
 	while (1)
 		pause();
 	return(0);
